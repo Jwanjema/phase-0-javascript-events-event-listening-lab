39264b46d832c23c0f2408bfe39ad7385d907e31
joewaweruwanjema <wawerujoe@gmail.com>
2025-06-15 17:20:03 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-function addingEventListener() {
-  // Get the button element
-  const button = document.getElementById('button');
-  
-  // Define the click handler function
-  function clickHandler() {
-    // This can remain empty for the test
-    // In a real application, you might put: alert('I was clicked!');
-  }
-  
-  // Add the event listener
-  button.addEventListener('click', clickHandler);
-  
-  // Return true to satisfy the test assertion
-  return true;
-}
-
-// Make the function available to both browser and test environments
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = addingEventListener;
-} else if (typeof window !== 'undefined') {
-  window.addingEventListener = addingEventListener;
+function addingEventListener () {
+        const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
